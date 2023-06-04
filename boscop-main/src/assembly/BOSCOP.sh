b59730798adb3cf0858d7b847f8259f58d226c5b
liturner <turnertech@outlook.de>
2023-06-04 08:24:09 +0200

    Update BOSCOP.sh

--- boscop-main/src/assembly/BOSCOP.sh
+++ boscop-main/src/assembly/BOSCOP.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 VM_OPTIONS="-Djava.util.logging.config.file=conf/logging.properties"
 DIR=`dirname $0`
+
+# The exec is important and makes the process run as a single parent 
+# thread, not a parent and child. (#11)
 exec java $VM_OPTIONS -jar $DIR/lib/boscop.jar "$@"
